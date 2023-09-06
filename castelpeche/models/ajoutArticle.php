1df68d5c1fe2775cb4145cd062d2d0092fe3a029
WillGRD <w.gerardin@outlook.fr>
2023-09-06 23:28:49 +0200

    update de ajout article

--- castelpeche/models/ajoutArticle.php
+++ castelpeche/models/ajoutArticle.php
@@ -5,6 +5,7 @@ $name = $_POST["titre"];
 $photo = $_POST["photo"];
 $desc = $_POST["description"];
 
+if (!empty($name) && !empty($photo) && !empty($desc)) {
 $requeteAjout = $database->prepare("INSERT INTO article(nom, image, description) VALUES (:nom, :image, :description)");
 $requeteAjout->bindParam('nom', $name);
 $requeteAjout->bindParam('image', $photo);
@@ -13,3 +14,8 @@ $requeteAjout->bindParam('description', $desc);
 $requeteAjout->execute();
 
 header('Location: ../templates/actualite.php');
+}
+
+else {
+    echo 'Vous devez renseigner quelque chose';
+}
