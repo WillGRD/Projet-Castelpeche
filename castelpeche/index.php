9625573500acd84bc834e3bf8de34ae1248d601a
Julien Sculteur <juliensculteur@gmail.com>
2023-09-16 16:37:15 +0200

    Mini Factorisation index.php

--- castelpeche/index.php
+++ castelpeche/index.php
@@ -1,5 +1,6 @@
 <?php
     define("RACINE", str_replace("index.php", "", $_SERVER["SCRIPT_FILENAME"]));
+    define("CONTROLLEUR_DIR", RACINE."controller/");
 
     $parametres = explode("/", $_GET["param"]);
 
@@ -8,7 +9,7 @@
         $action = isset($parametres[1]) ? $parametres[1] : 'index';
         $args = isset($parametres[2]) ? $parametres[2] : null;
 
-        require_once(RACINE.'controller/'.$controller.'.php');
+        require_once(CONTROLLEUR_DIR.$controller.'.php');
         if($args) {
             $action($args);
         } else {
@@ -16,6 +17,6 @@
         }
         
     } else {
-        require_once(RACINE.'controller/Accueil.php');
+        require_once(CONTROLLEUR_DIR.'Accueil.php');
         index();
     }
